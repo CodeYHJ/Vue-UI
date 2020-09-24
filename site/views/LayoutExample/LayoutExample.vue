b64c6c86ea5692dbc6b725fddb8d1f7e48789dde
CodeYHJ <colgateyhj@gmail.com>
2020-09-24 16:44:56 +0800

    LayoutExample 初始版

--- site/views/LayoutExample/LayoutExample.vue
+++ site/views/LayoutExample/LayoutExample.vue
@@ -19,6 +19,53 @@
       <p :class="cls('p')"><strong>Content</strong> ：可嵌套任何元素</p>
       <p :class="cls('p')"><strong>Footer</strong> ：只可嵌套任何元素</p>
     </section>
+    <section>
+      <h2 :class="cls('title')">代码示例</h2>
+      <ExampleBox
+        :class="cls('exampleBox')"
+        title="基本结构"
+        description="页面布局"
+        :code="baseCodeStr"
+      >
+        <section>
+          <Layout style="color:#ffffff ">
+            <Header :class="cls('header')">Header</Header>
+            <Content :class="cls('content')">Content</Content>
+            <Footer :class="cls('footer')">Footer</Footer>
+          </Layout>
+        </section>
+        <section>
+          <Layout style="color: #ffffff">
+            <Header :class="cls('header')">Header</Header>
+            <Layout style="color: #ffffff">
+              <Aside :class="cls('aside')">Aside</Aside>
+              <Content :class="cls('content')">Content</Content>
+            </Layout>
+            <Footer :class="cls('footer')">Footer</Footer>
+          </Layout>
+        </section>
+        <section>
+          <Layout style="color: #ffffff">
+            <Header :class="cls('header')">Header</Header>
+            <Layout style="color: #ffffff">
+              <Content :class="cls('content')">Content</Content>
+              <Aside :class="cls('aside')">Aside</Aside>
+            </Layout>
+            <Footer :class="cls('footer')">Footer</Footer>
+          </Layout>
+        </section>
+        <section>
+          <Layout style="color: #ffffff">
+            <Aside :class="cls('aside')">Aside</Aside>
+            <Layout style="color: #ffffff">
+              <Header :class="cls('header')">Header</Header>
+              <Content :class="cls('content')">Content</Content>
+              <Footer :class="cls('footer')">Footer</Footer>
+            </Layout>
+          </Layout>
+        </section>
+      </ExampleBox>
+    </section>
   </div>
 </template>
 
@@ -27,13 +74,24 @@ import { defineComponent } from 'vue';
 
 import { createClass } from '../../util';
 
+import ExampleBox from '../../components/ExampleBox/ExampleBox.vue';
+
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const baseCode = require('!!raw-loader!./code/BaseCode.vue');
+
+import Layout from '@/Layout';
+
+const { Header, Content, Footer, Aside } = Layout;
+
 export default defineComponent({
   name: 'LayoutExample',
-
+  components: { Layout, Header, Content, Footer, Aside, ExampleBox },
   setup() {
     const cls = createClass('layout');
 
-    return { cls };
+    const baseCodeStr = baseCode && baseCode.default;
+
+    return { cls, baseCodeStr };
   }
 });
 </script>
---
+++ site/views/LayoutExample/code/BaseCode.vue
@@ -0,0 +1,57 @@
+<template>
+  <div>
+    <div>
+      <Layout>
+        <Header>Header</Header>
+        <Content>Content</Content>
+        <Footer>Footer</Footer>
+      </Layout>
+    </div>
+
+    <div>
+      <Layout>
+        <Header>Header</Header>
+        <Layout>
+          <Aside>Aside</Aside>
+          <Content>Content</Content>
+        </Layout>
+        <Footer>Footer</Footer>
+      </Layout>
+    </div>
+
+    <div>
+      <Layout>
+        <Header>Header</Header>
+        <Layout>
+          <Content>Content</Content>
+          <Aside>Aside</Aside>
+        </Layout>
+        <Footer>Footer</Footer>
+      </Layout>
+    </div>
+
+    <div>
+      <Layout>
+        <Aside>Aside</Aside>
+        <Layout>
+          <Header>Header</Header>
+          <Content>Content</Content>
+          <Footer>Footer</Footer>
+        </Layout>
+      </Layout>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+import Layout from '@/Layout';
+
+const { Header, Content, Footer, Aside } = Layout;
+
+export default defineComponent({
+  name: 'BaseCode',
+  components: { Layout, Header, Content, Footer, Aside }
+});
+</script>
