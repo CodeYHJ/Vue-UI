6cadc792762e81faf22527732af19f1188ae6e99
CodeYHJ <colgateyhj@gmail.com>
2020-09-24 11:33:19 +0800

    Icon 组件基础搭建

---
+++ components/Icon/LocalIcon.vue
@@ -0,0 +1,29 @@
+<template>
+  <svg :class="cls" {...others}>
+    <use :xlinkHref="`#${name}`" />
+  </svg>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+import { createClass } from '@/util';
+
+interface LocalIconProps {
+  name: string;
+}
+
+export default defineComponent({
+  name: 'LocalIcon',
+  props: { name: { type: String, require: true } },
+  setup() {
+    const cls = createClass('svg');
+
+    return { cls };
+  }
+});
+</script>
+
+<style lang="less">
+@import './style/index';
+</style>
---
+++ components/Icon/createIcon.tsx
@@ -0,0 +1,23 @@
+import Icon from './LocalIcon.vue';
+
+export interface CreateIconProps {
+  scriptUrl: string;
+}
+
+const setMap = new Set();
+
+const createIcon = (scriptUrl: string) => {
+  if (!setMap.has(scriptUrl)) {
+    const svgDom = document.createElement('script');
+
+    svgDom.setAttribute('src', scriptUrl);
+
+    document.body.appendChild(svgDom);
+
+    setMap.add(scriptUrl);
+  }
+
+  return Icon;
+};
+
+export default createIcon;
